c7679aa5adf359a737fbca49646258abf5b8a3da
msimalunsal <msimalunsal@gmail.com>
2021-04-08 02:12:35 +0300

    Obstacle Detector double door target bug fixed.

--- Assets/[Game]/Scripts/AI/ObstacleDetector.cs
+++ Assets/[Game]/Scripts/AI/ObstacleDetector.cs
@@ -48,11 +48,12 @@ public class ObstacleDetector: Singleton<ObstacleDetector>
             {
                 if(transform.position.z <= targetList[a].transform.position.z && Vector3.Distance(transform.position , targetList[a].transform.position) < 5f) 
                 {
-                    if((targetList[a].transform.position.x - transform.position.x < 1f && targetList[a].transform.parent.parent.GetComponent<Target>().targetSituation == Target.TargetSituation.close)
-                    || (targetList[a].transform.position.x - transform.position.x >= 1f && targetList[a].transform.parent.parent.GetComponent<Target>().targetSituation == Target.TargetSituation.open))
+                    if((targetList[a].transform.position.x - transform.position.x < 1.5f && targetList[a].transform.parent.parent.GetComponent<Target>().targetSituation == Target.TargetSituation.close && targetList[a].transform.parent.parent.name != "DoubleDoor") 
+                    || (targetList[a].transform.position.x - transform.position.x >= 1.5f && targetList[a].transform.parent.parent.GetComponent<Target>().targetSituation == Target.TargetSituation.open))
                     { // eger kendi hedefiyse ve hedef kapalıysa vur.
                         // ya da playerın hedefiyse ve onun hedefi açıksa vur.
-                        Vector3 direction = new Vector3(targetList[a].transform.position.x,targetList[a].transform.position.y + 1.5f,targetList[a].transform.position.z);
+                        Debug.Log(targetList[a].transform.parent.parent.name);
+                        Vector3 direction = new Vector3(targetList[a].transform.position.x +1f,targetList[a].transform.position.y + 1.5f,targetList[a].transform.position.z);
                         _ball.Push(direction);
                         RandomTime();
                         canThrow = false;
